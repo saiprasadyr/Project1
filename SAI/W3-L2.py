78fd7a71dd7f03cefdbfb5dc0138eee1e10ef77e
saiprasadyr <saiprasadyr18@gmail.com>
2024-08-25 12:42:13 +0530

    Add files via upload

---
+++ SAI/W3-L2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  6 05:26:09 2023
+
+@author: saipr
+"""
+
+_num1=3
+_num2=3
+
+print(_num1+_num2)
---
+++ SAI/untitled1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  6 04:43:30 2023
+
+@author: saipr
+"""
+
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+c=a+b
+print("sum is")
+print(c)
